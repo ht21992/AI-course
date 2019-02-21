defa0c23b8619aac1fb121baa7247b83881c90ac
ht21992 <ht21992@gmail.com>
2019-02-21 14:07:35 +0400

    chat bot with learn method

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import random
 class ChatBot:
-    learning={}
     def __init__(self,standardanswer,questionanswer):
         self.standardanswer=standardanswer
         self.questionanswer=questionanswer
@@ -11,22 +10,22 @@ class ChatBot:
         else:
             random_ans=random.randint(0,len(self.standardanswer)-1)
             print(self.standardanswer[random_ans])
-    def learn(self):
-        print("what is your name?")
-        name=input()
-        ChatBot.learning["name"]=name
-        print("what is your lastname?")
-        lastname=input()
-        ChatBot.learning["lastname"]=lastname
-        print("nice to meet you "+ChatBot.learning["name"]+" "+ChatBot.learning["lastname"])
+    def learn(self,text):
+        if "?" in text:
+            self.questionanswer.append(text)
+        else:
+            self.standardanswer.append(text)
+
+
+
 
 Question_ans=["it's rainy","it's sunny"]
 standard_ans=["hello","hi"]
 chat1=ChatBot(standard_ans,Question_ans)
-
-while True:
-    ans = input()
-    chat1.reply(ans)
+chat1.learn("i do not know")
+#while True:
+    #ans = input()
+    #chat1.reply(ans)
 
 
 #chat1.learn()
