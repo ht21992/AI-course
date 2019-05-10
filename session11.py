f410ad24fb82a96dd0faaa6d82b1e8bc7ee646b4
ht21992 <ht21992@gmail.com>
2019-05-10 08:48:14 +0500

    session 11

---
+++ session11.py
@@ -0,0 +1,3 @@
+import pandas as pd
+white=pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine.quality/winequality-white.csv')
+red=d.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine.quality/winequality-red.csv')
